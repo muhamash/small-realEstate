73d6057f6395e32f93e79ef0c5f77c5fcd940d37
Muhammad Ashraful Alam <muhamash@Muhammads-Mac-mini.local>
2024-10-06 19:55:15 +0600

    about  on

--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 // import React from 'react'
 
-export default function Title({color, text}) {
+export default function Title({color, text, children}) {
     return (
         <p style={ {
             color : color ? color : '#4B5563'
         }} className="text-base  dark:text-dark-6">
-            {text ? text :' There are many variations of passages of Lorem'}
+            { text ? text : ' There are many variations of passages of Lorem' }
+            {children}
         </p>
     );
 }
